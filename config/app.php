9b0493a24690d4d1b9a0d311f574100df528dcad
Sachin <skad5455[at]gmail[dot]com>
2016-11-05 11:22:17 +0530

    removed faulty database

--- config/app.php
+++ config/app.php
@@ -177,9 +177,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-	
-	Illuminate\Html\HtmlServiceProvider::class,
+        App\Providers\RouteServiceProvider::class
     ],
 
     /*
@@ -227,10 +225,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Entrust'   => Zizaco\Entrust\EntrustFacade::class,
-        'MaterializeCSS' => Skydiver\LaravelMaterializeCSS\MaterializeCSS::class,
-	'Form'=> 'Illuminate\Html\FormFacade',
-	'HTML'=> 'Illuminate\Html\HtmlFacade'
-
+        'MaterializeCSS' => Skydiver\LaravelMaterializeCSS\MaterializeCSS::class
     ],
 
 ];
--- resources/views/time_table_management/scheduleanextraclass.blade.php
+++ resources/views/time_table_management/scheduleanextraclass.blade.php
@@ -13,29 +13,29 @@
     </ul>
   </div>
 </nav>
-  {!! Form::open(array('route'=> 'schedule.store')) !!}
+  <form method="post" action='schedule.store'>
   <div class="row">
       <div class="col s6 l2 m4">
-      <?php echo Form::label('bookingdate','Booking For');
-        echo Form::input('date', 'bookingdate',null,array('class'=>'validate')); ?>
+      <label for='bookingdate'>Booking For</label>
+      <input type='date' name="bookingdate" id='bookingdate' class='validate'>
       </div>
       <div class="col s6 l2 m4">
-      <?php echo Form::label('StartTime','Start Time'); ?>
-      <select name="StartTime"> <option value="" disabled selected>Start Time</option> <option value="09:00:00">9:00</option> <option value="10:00:00">10:00</option> <option value="11:00:00">11:00</option> <option value="12:00:00">12:00</option><option value="14:30:00">2:30</option><option value="15:30:00">3:30</option><option value="15:30:00">4:30</option></select>
+      <label for='StartTime'>Start Time</label>
+      <select name="StartTime" id="StartTime"> <option value="" disabled selected>Start Time</option> <option value="09:00:00">9:00</option> <option value="10:00:00">10:00</option> <option value="11:00:00">11:00</option> <option value="12:00:00">12:00</option><option value="14:30:00">2:30</option><option value="15:30:00">3:30</option><option value="15:30:00">4:30</option></select>
       </div>
       <div class="col s6 l2 m4">
-      <?php echo Form::label('EndTime','End Time'); ?>
-        <select name="EndTime"> <option value="" disabled selected>End Time</option> <option value="10:00:00">10:00</option> <option value="11:00:00">11:00</option> <option value="12:00:00">12:00</option><option value="14:30:00">2:30</option><option value="15:30:00">3:30</option><option value="15:30:00">4:30</option> <option value="17:30:00">5:30</option> </select>
+      <label for='EndTime'>End Time</label>
+        <select name="EndTime" id="EndTime"> <option value="" disabled selected>End Time</option> <option value="10:00:00">10:00</option> <option value="11:00:00">11:00</option> <option value="12:00:00">12:00</option><option value="14:30:00">2:30</option><option value="15:30:00">3:30</option><option value="15:30:00">4:30</option> <option value="17:30:00">5:30</option> </select>
         </div>
         <div class="col s6 l3 m4">
-      <?php echo Form::label('CourseCode','Course Code'); ?>
-      <?php echo Form::text('CourseCode',null,array('class'=>'validate')); ?>
+      <label for='CourseCode'>Course Code</label>
+      <input type="text" name="CourseCode" id='CourseCode' class='validate'>
       </div>
         <div class="col s6 l3 m4">
-      <?php echo Form::label('Strength','Strength'); ?>
-      <?php echo Form::input('number','Strength',null,array('class'=>'validate')); ?>
+      <label for='Strength'>Strength</label>
+      <input type='number' name='Strength' class='validate'>
       </div>
       </div>
-      <?php echo Form::submit('Submit' ,array('class'=>'waves-effect btn-flat')); ?>
-      {!! Form::close() !!} 
+      <input type='submit' value='Submit' class='waves-effect btn-flat'>
+      </form>
     @stop
